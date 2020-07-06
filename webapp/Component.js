c6a487de6feaf77f1c1f8bd3d7c3a3c513d88758
QuanTranDoanAnh <td.anhquan@gmail.com>
2020-07-06 14:53:27 +0700

    Step 10: Descriptor (manifest.json)

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,18 +1,14 @@
 sap.ui.define([
     "sap/ui/core/UIComponent",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
+    "sap/ui/model/json/JSONModel"
 ], function(UIComponent, JSONModel, ResourceModel) {
     "use strict";
 
     return UIComponent.extend("quantda.fiori.ui5demo.walkthrough.Component", {
         metadata : {
-            rootView: {
-               "viewName": "quantda.fiori.ui5demo.walkthrough.view.App",
-               "type": "XML",
-               "async": true,
-               "id": "app"
-            }
+            metadata : {
+                manifest: "json"
+            },
          },
 
         init: function() {
@@ -26,12 +22,6 @@ sap.ui.define([
             };
             var oModel = new JSONModel(oData);
             this.setModel(oModel);
-
-            // set i18n model
-            var i18nModel = new ResourceModel({
-                bundleName : "quantda.fiori.ui5demo.walkthrough.i18n.i18n"
-            });
-            this.setModel(i18nModel, "i18n");
         }
     });
     
